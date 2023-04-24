c8648c001e4d554b06a7fbe6cc8d463f92691273
Samyuktha <samyuj15@gmail.com>
2023-04-24 15:54:22 +0530

    Add initial set of test cases

--- src/test/java/org/equalexperts/com/ShoppingCartTest.java
+++ src/test/java/org/equalexperts/com/ShoppingCartTest.java
@@ -1,7 +1,75 @@
 package org.equalexperts.com;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShoppingCartTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void shouldAddProductToCart() {
+
+    }
+
+    @Test
+    void shouldSetPriceToProduct() {
+
+    }
+
+    @Test
+    void handleProductNotPresentFromAPI() {
+
+    }
+
+    @Test
+    void shouldUpdateCartState() {
+
+    }
+
+    @Test
+    void shouldIgnoreProductInCartStateWhenPriceNotPresent() {
+
+    }
+
+    @Test
+    void shouldUpdateCartStateForEveryProductAddition() {
+
+    }
+
+    @Test
+    void shouldFetchProductPriceFromAPI() {
+
+    }
+
+    @Test
+    void shouldRoundUpTaxPayable() {
+
+    }
+
+    @Test
+    void shouldRoundUpTotalPayable() {
+
+    }
+
+    @Test
+    void shouldCalculateTotalPayable() {
+
+    }
+
+    @Test
+    void shouldCalculateTaxPayable() {
+
+    }
+
+    @Test
+    void shouldCalculateCartSubtotal() {
+
+    }
 
 }
