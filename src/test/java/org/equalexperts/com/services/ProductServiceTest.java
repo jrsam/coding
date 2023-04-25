530e74db95ff1573b276a6d57b68201663c8048d
Samyuktha <samyuj15@gmail.com>
2023-04-25 11:42:22 +0530

    gc -m "test: handle product not found scenario"

--- src/test/java/org/equalexperts/com/services/ProductServiceTest.java
+++ src/test/java/org/equalexperts/com/services/ProductServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ProductServiceTest {
 
@@ -14,6 +15,14 @@ class ProductServiceTest {
         Product product = new Product("cornflakes", 2);
         ProductService productService = new ProductService();
         assertEquals(2.52, productService.getProductPrice(product.getName()));
+    }
+
+
+    @Test
+    void handleProductNotPresentFromAPI() {
+        Product product = new Product("raspberries", 2);
+        ProductService productService = new ProductService();
+        assertThrows(ProductNotFoundException.class, () -> productService.getProductPrice(product.getName()));
     }
 
 }
