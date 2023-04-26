1e47df32563c958d48d564cf66b676ed1d733f67
Samyuktha <samyuj15@gmail.com>
2023-04-26 12:53:53 +0530

    refactor: use mocks to simulate external API calls

--- src/test/java/org/equalexperts/com/services/ProductServiceTest.java
+++ src/test/java/org/equalexperts/com/services/ProductServiceTest.java
@@ -1,28 +1,53 @@
 package org.equalexperts.com.services;
 
-import org.equalexperts.com.Product;
 import org.equalexperts.com.exceptions.ProductNotFoundException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-
+@ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
 
+    @Mock
+    private static HttpClient httpClient;
+
+    @InjectMocks
+    ProductService productService;
+
+    @Mock
+    private static HttpResponse httpResponse;
+
     @Test
-    void shouldFetchProductPriceFromAPI() throws ProductNotFoundException {
-        Product product = new Product("cornflakes", 2);
-        ProductService productService = new ProductService();
-        assertEquals(2.52, productService.getProductPrice(product.getName()));
+    void shouldFetchProductPriceFromAPI() throws ProductNotFoundException, IOException, InterruptedException {
+        when(httpClient.send(any(HttpRequest.class), any(HttpResponse.BodyHandler.class))).thenReturn(httpResponse);
+        when(httpResponse.statusCode()).thenReturn(200);
+
+        when(httpResponse.body()).thenReturn("{\n" +
+                "  \"title\": \"Corn Flakes\",\n" +
+                "  \"price\": 2.52\n" +
+                "}\n");
+
+        assertEquals(2.52, productService.getProductPrice("cornflakes"));
     }
 
-
     @Test
-    void handleProductNotPresentFromAPI() {
-        Product product = new Product("raspberries", 2);
-        ProductService productService = new ProductService();
-        assertThrows(ProductNotFoundException.class, () -> productService.getProductPrice(product.getName()));
+    void handleProductNotPresentFromAPI() throws IOException, InterruptedException {
+        when(httpClient.send(any(HttpRequest.class), any(HttpResponse.BodyHandler.class))).thenReturn(httpResponse);
+        when(httpResponse.statusCode()).thenReturn(500);
+
+        assertThrows(ProductNotFoundException.class, () -> productService.getProductPrice("chocos"));
     }
 
 }
