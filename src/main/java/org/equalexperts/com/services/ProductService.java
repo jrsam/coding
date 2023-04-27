eca01f0f2e63a657f25006aa99fa88cdd8a5d60b
Samyuktha <samyuj15@gmail.com>
2023-04-27 16:02:57 +0530

    refactor: productService refactor

--- src/main/java/org/equalexperts/com/services/ProductService.java
+++ src/main/java/org/equalexperts/com/services/ProductService.java
@@ -12,24 +12,21 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ProductService {
-    private final String baseUrl = "https://equalexperts.github.io/";
-    private final String endPoint = "backend-take-home-test-data/";
+    private final String endPoint = "https://equalexperts.github.io/backend-take-home-test-data/";
     HttpClient httpClient = HttpClient.newHttpClient();
-    ObjectMapper objectMapper;
 
     public double getProductPrice(String name) throws ProductNotFoundException {
-        HttpRequest httpRequest;
         ProductResponse productResponse;
         try {
-            httpRequest = HttpRequest.newBuilder()
-                    .uri(new URI(baseUrl + endPoint + name + ".json"))
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(new URI(endPoint + name + ".json"))
                     .build();
 
             HttpResponse httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
             if (httpResponse.statusCode() != 200) {
                 throw new ProductNotFoundException("Product " + name + " not found");
             }
-            objectMapper = new ObjectMapper();
+            ObjectMapper objectMapper = new ObjectMapper();
             productResponse = objectMapper.readValue(httpResponse.body().toString(), ProductResponse.class);
         } catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e.getMessage());
